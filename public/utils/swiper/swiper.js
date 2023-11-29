aafd29ed64d31363ed34f1e381df11f581853d51
nikita petrov <petrov.nikita702@mail.ru>
2023-11-29 05:54:32 +0300

    Вёрстка 2 и 3 блока для display

--- public/utils/swiper/swiper.js
+++ public/utils/swiper/swiper.js
@@ -1,6 +1,8 @@
 const swiper = new Swiper('.swiper', {
   slidesPerView: "auto",
-
+  autoplay: {
+    delay: 5000,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
